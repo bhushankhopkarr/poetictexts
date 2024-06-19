84657868c5af49a23172de304d671ddd96826f45
bhushankhopkarr <bhushankhopkar06@gmail.com>
2024-06-19 10:28:08 +0530

    modified file: main.py

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Activation
 from tensorflow.keras.optimizers import RMSprop
 
+# Download the Shakespeare text file
 filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespear.txt')
 
+# Read the file
 text = open(filepath, 'rb').read().decode(encoding='utf-8').lower()
 
+# Use a subset of the text
 text = text[300000:800000]
 
+# Create a sorted list of unique characters in the text
 characters = sorted(set(text))
 
+# Create dictionaries to map characters to indices and vice versa
 char_to_index = dict((c, i) for i, c in enumerate(characters))
 index_to_char = dict((i, c) for i, c in enumerate(characters))
 
@@ -22,25 +27,32 @@ STEP_SIZE = 3
 sentences = []
 next_characters = []
 
+# Create training examples
 for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
     sentences.append(text[i: i + SEQ_LENGTH])
     next_characters.append(text[i + SEQ_LENGTH])
 
-x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype= np.bool)
-y = np.zeros((len(sentences), len(characters)), dtype= np.bool)
+# Initialize input and output tensors
+x = np.zeros((len(sentences), SEQ_LENGTH, len(characters)), dtype=np.bool_)
+y = np.zeros((len(sentences), len(characters)), dtype=np.bool_)
 
+# Populate the tensors with one-hot encoded data
 for i, sentence in enumerate(sentences):
     for t, character in enumerate(sentence):
         x[i, t, char_to_index[character]] = 1
     y[i, char_to_index[next_characters[i]]] = 1
 
+# Build the model
 model = Sequential()
 model.add(LSTM(128, input_shape=(SEQ_LENGTH, len(characters))))
 model.add(Dense(len(characters)))
 model.add(Activation('softmax'))
 
-model.compile(loss= 'categorical crossentropy', optimizer = RMSprop(lr= 0.01))
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(learning_rate=0.01))
 
-model.fit(x, y, batch_size = 256, epochs = 4)
+# Train the model
+model.fit(x, y, batch_size=256, epochs=4)
 
+# Save the model
 model.save('textgenerator.model')
