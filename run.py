1f78e241b7cd67f7ca6eb8de23403167c0940c68
Bhushan Khopkar <bhushankhopkar06@gmail.com>
2024-06-19 13:01:46 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,47 +1,39 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Activation
-from tensorflow.keras.optimizers import RMSprop
 
-# Read the file
-text = open(filepath, 'rb').read().decode(encoding='utf-8').lower()
-
-# Use a subset of the text
-text = text[300000:800000]
-
-# Create a sorted list of unique characters in the text
-characters = sorted(set(text))
-
-# Create dictionaries to map characters to indices and vice versa
-char_to_index = dict((c, i) for i, c in enumerate(characters))
-index_to_char = dict((i, c) for i, c in enumerate(characters))
-
-SEQ_LENGTH = 40
-STEP_SIZE = 3
-
+# Load model
 model = tf.keras.models.load_model('textgenerator.model/textgenerator.h5')
 
-#text generation function
+# Read the text file
+filepath = tf.keras.utils.get_file('shakespeare.txt',
+                                   'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+text = open(filepath, 'rb').read().decode('utf-8').lower()[300000:800000]
+
+# Create character mappings
+characters = sorted(set(text))
+char_to_index = {c: i for i, c in enumerate(characters)}
+index_to_char = {i: c for i, c in enumerate(characters)}
+
+SEQ_LENGTH = 40
+
+# Sampling function
 def sample(preds, temperature=1.0):
     preds = np.asarray(preds).astype('float64')
     preds = np.log(preds) / temperature
     exp_preds = np.exp(preds)
     preds = exp_preds / np.sum(exp_preds)
-    probas = np.random.multinomial(1, preds, 1)
-    return np.argmax(probas)
+    return np.argmax(np.random.multinomial(1, preds, 1))
 
-# Function to generate text
+# Text generation function
 def generate_text(model, text, char_to_index, index_to_char, length=100, temperature=1.0):
-    SEQ_LENGTH = 40
     start_index = np.random.randint(0, len(text) - SEQ_LENGTH - 1)
-    sentence = text[start_index: start_index + SEQ_LENGTH]
+    sentence = text[start_index:start_index + SEQ_LENGTH]
     generated = sentence
-    
+
     for _ in range(length):
         x = np.zeros((1, SEQ_LENGTH, len(char_to_index)))
-        for t, character in enumerate(sentence):
-            x[0, t, char_to_index[character]] = 1
+        for t, char in enumerate(sentence):
+            x[0, t, char_to_index[char]] = 1
 
         predictions = model.predict(x, verbose=0)[0]
         next_index = sample(predictions, temperature)
@@ -52,13 +44,9 @@ def generate_text(model, text, char_to_index, index_to_char, length=100, tempera
 
     return generated
 
-print('---------0.2---------')
-print(generate_text(300, 0.2))
-print('---------0.4---------')
-print(generate_text(300, 0.4))
-print('---------0.6---------')
-print(generate_text(300, 0.6))
-print('---------0.8---------')
-print(generate_text(300, 0.8))
-print('---------1---------')
-print(generate_text(300, 1.0))
+# Generate and print text with different temperatures
+temperatures = [0.2, 0.4, 0.6, 0.8, 1.0]
+for temp in temperatures:
+    print(f'--------- Temperature {temp} ---------')
+    print(generate_text(model, text, char_to_index, index_to_char, length=300, temperature=temp))
+    print('-' * 80)
